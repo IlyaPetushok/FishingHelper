4aafe41066a2ff35cf9f0701e44d3d69e20809e2
Petushok Ilya <a3310010752@mail.ru>
2024-07-30 17:03:11 +0300

    add request-mapping

--- fishing-helper/auth-service/src/main/java/fishinghelper/auth_service/controller/RegistrationController.java
+++ fishing-helper/auth-service/src/main/java/fishinghelper/auth_service/controller/RegistrationController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/auth")
 @RestController
 public class RegistrationController {
     public final RegistrationService registrationService;
