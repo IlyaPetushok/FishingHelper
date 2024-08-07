2d1496e67218436592793d4c5277c0489df62cef
Petushok Ilya <a3310010752@mail.ru>
2024-08-07 12:38:56 +0300

    update providerJwt for protocol oauth2

--- fishing-helper/security-server/src/main/java/fishinghelper/security_server/util/JwtProvider.java
+++ fishing-helper/security-server/src/main/java/fishinghelper/security_server/util/JwtProvider.java
@@ -1,62 +1,123 @@
 package fishinghelper.security_server.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Collection;
-import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 @Service
 public class JwtProvider implements Converter<Jwt, AbstractAuthenticationToken> {
-    @Value("${jwt.secret}")
-    private String secretKey;
-
-    @Value("${jwt.time}")
-    private String timeExpire;
-
-    @Value("${jwt.refresh.time}")
-    private String timeRefreshExpire;
-
-    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =  new JwtGrantedAuthoritiesConverter();
-
-    @Value("${spring.security.oauth2.client.provider.keycloak.user-name-attribute}")
-    private String principleAttribute;
     @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
     private String clientId;
 
-//    @Autowired
-//    public JwtProvider(JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter) {
-//        this.jwtGrantedAuthoritiesConverter = jwtGrantedAuthoritiesConverter;
-//    }
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.url.token}")
+    private String urlOpenIdToken;
+
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.user-name-attribute}")
+    private String principleAttribute;
+
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String urlRealm;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+    private final CustomUserDetailService customUserDetailService;
+
+    @Autowired
+    public JwtProvider(CustomUserDetailService customUserDetailService) {
+        this.customUserDetailService = customUserDetailService;
+    }
 
     @Override
     public AbstractAuthenticationToken convert(@NotNull Jwt jwt) {
+        checkActiveSession(jwt);
+        String login = jwt.getClaimAsString(principleAttribute);
+
         Collection<GrantedAuthority> authorities = Stream.concat(
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractResourceRoles(jwt).stream()
+                customUserDetailService.loadUserByUsername(login).getAuthorities().stream()
         ).collect(Collectors.toSet());
+
         return new JwtAuthenticationToken(
                 jwt,
                 authorities,
                 getPrincipleClaimName(jwt)
         );
+    }
+
+    public void checkActiveSession(Jwt jwt) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("client_id", clientId);
+        body.add("client_secret", clientSecret);
+        body.add("token", jwt.getTokenValue());
+
+        HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(urlOpenIdToken + "/introspect", requestEntity, String.class);
+
+        handleResponse(response);
+    }
+
+    private void handleResponse(ResponseEntity<String> response) {
+        if (response.getStatusCode() == HttpStatus.OK) {
+            processResponseBody(response.getBody());
+        } else {
+            throw new RuntimeException("Failed to introspect token: " + response.getStatusCode());
+        }
+    }
+
+    private void processResponseBody(String responseBody) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+            boolean isActive = jsonNode.get("active").asBoolean();
+
+            if (isActive) {
+                System.out.println("Token is active.");
+            } else {
+                System.out.println("Token is inactive or invalid.");
+                throw new RuntimeException("Token is inactive or invalid.");
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to process token introspection response.", e);
+        }
+    }
+
+    public String getLoginFromJwt(String token) {
+        JwtDecoder jwtDecoder = JwtDecoders.fromIssuerLocation(urlRealm);
+        Jwt jwt = jwtDecoder.decode(token);
+        return jwt.getClaimAsString(principleAttribute);
     }
 
     private String getPrincipleClaimName(Jwt jwt) {
@@ -67,103 +128,4 @@ public class JwtProvider implements Converter<Jwt, AbstractAuthenticationToken> 
         return jwt.getClaim(claimName);
     }
 
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String, Object> resourceAccess;
-        Map<String, Object> resource;
-        Collection<String> resourceRoles;
-
-        if (Objects.isNull(jwt.getClaim("resource_access"))) {
-            return Set.of();
-        }
-        resourceAccess = jwt.getClaim("resource_access");
-        if (Objects.isNull(resourceAccess.get(clientId))) {
-            return Set.of();
-        }
-        resource = (Map<String, Object>) resourceAccess.get(clientId);
-
-        resourceRoles = (Collection<String>) resource.get("roles");
-        return resourceRoles.stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(Collectors.toSet());
-    }
-
-    //    /**
-//     * Generates a refresh token for the specified user.
-//     *
-//     * @param user The User object for which the refresh token is generated.
-//     * @return The generated refresh token.
-//     */
-//    public String generateRefreshToken(User user){
-//        log.info("generate refresh token ...");
-//        return buildToken(user,timeRefreshExpire);
-//    }
-//
-//    /**
-//     * Generates an access token for the specified user.
-//     *
-//     * @param user The User object for which the access token is generated.
-//     * @return The generated access token.
-//     */
-//    public String generateToken(User user){
-//        log.info("generate acess token ...");
-//        return buildToken(user,timeExpire);
-//    }
-//
-//    /**
-//     * Builds a JWT token for the specified user with the given expiration time.
-//     *
-//     * @param user The User object for which the token is built.
-//     * @param time The expiration time (in minutes) for the token.
-//     * @return The generated JWT token as a String.
-//     */
-//    public String buildToken(User user,String time){
-//        Date date = DateUtils.addMinutes(new Date(), Integer.parseInt(time));
-//        Claims claims =  null;
-////                Jwts.claims().setSubject(user.getLogin());
-//        claims.put("role", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()));
-//        claims.put("name", user.getName());
-//        return Jwts.builder()
-//                .setClaims(claims)
-//                .setExpiration(date)
-//                .signWith(getSignedKey())
-//                .compact();
-//    }
-//
-//    /**
-//     * Validates the provided JWT token.
-//     *
-//     * @param token The JWT token to be validated.
-//     * @return true if the token is valid (not expired), false otherwise.
-//     */
-//    public boolean validateToken(String token) {
-////        Jws<Claims> claimsJws=Jwts.parserBuilder().setSigningKey(getSignedKey()).build().parseClaimsJws(token);
-////        return !claimsJws.getBody().getExpiration().before(new Date());
-//        return false;
-//    }
-//
-//    /**
-//     * Retrieves the login (subject) from the provided JWT token.
-//     *
-//     * @param token The JWT token from which to retrieve the login.
-//     * @return The login (subject) extracted from the JWT token.
-//     */
-//    public String getLogin(String token) {
-//        Claims claims = null;
-////                Jwts.parserBuilder()
-////                .setSigningKey(getSignedKey())
-////                .build()
-////                .parseClaimsJws(token)
-////                .getBody();
-//        return claims.getSubject();
-//    }
-//
-//    /**
-//     * Retrieves the signing key used for JWT token validation.
-//     *
-//     * @return The signing key as a Key object.
-//     */
-//    private Key getSignedKey(){
-//        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
-//        return Keys.hmacShaKeyFor(keyBytes);
-//    }
 }
