01dbc2902d5509cb9240a5966dfcb35ffec34bb0
Petushok Ilya <a3310010752@mail.ru>
2024-08-08 00:50:31 +0300

    add send html form on email

--- fishing-helper/notification-service/src/main/java/fishinghelper/notification_service/service/EmailService.java
+++ fishing-helper/notification-service/src/main/java/fishinghelper/notification_service/service/EmailService.java
@@ -1,9 +1,13 @@
 package fishinghelper.notification_service.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -40,8 +44,25 @@ public class EmailService {
         message.setFrom("a3310010752@mail.ru");
         message.setSubject(subject);
         message.setText(body);
-
         javaMailSender.send(message);
+        log.info("send message to "+ mail);
+    }
+
+
+    public void sendMessageHtml(String mail,String subject,String htmlContent){
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper ;
+        try {
+            helper = new MimeMessageHelper(message, true);
+            message.setFrom("a3310010752@mail.ru");
+            helper.setTo(mail);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true);
+
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
         log.info("send message to "+ mail);
     }
 }
