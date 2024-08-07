3de33ab55d5e5e84a0c28ac6bbd31ebee1ad031c
Petushok Ilya <a3310010752@mail.ru>
2024-08-07 14:21:05 +0300

    update

--- fishing-helper/security-server/src/main/java/fishinghelper/security_server/service/KeyCloakService.java
+++ fishing-helper/security-server/src/main/java/fishinghelper/security_server/service/KeyCloakService.java
@@ -1,7 +1,10 @@
 package fishinghelper.security_server.service;
 
 import fishinghelper.common_module.entity.user.User;
+import fishinghelper.security_server.exception.UserFailedCreateException;
+import fishinghelper.security_server.exception.UserNotFoundException;
 import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
@@ -10,12 +13,21 @@ import org.keycloak.representations.idm.RoleRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+
+/**
+ * Service class for managing users in Keycloak.
+ * Handles user creation, password updates, and email verification.
+ * <p>
+ * Author: Ilya Petushok
+ */
+@Slf4j
 @Service
 public class KeyCloakService {
     @Value("${spring.security.keycloak.realm.name}")
@@ -28,6 +40,12 @@ public class KeyCloakService {
         this.keycloak = keycloak;
     }
 
+    /**
+     * Creates a password credentials representation.
+     *
+     * @param password The password.
+     * @return The CredentialRepresentation object with the password.
+     */
     private CredentialRepresentation createPasswordCredentials(String password){
         CredentialRepresentation credentialRepresentation = new CredentialRepresentation();
         credentialRepresentation.setTemporary(false);
@@ -36,6 +54,13 @@ public class KeyCloakService {
         return credentialRepresentation;
     }
 
+    /**
+     * Adds a new user to Keycloak and assigns a role.
+     * <p>
+     * If the user creation fails, throws a {@link UserFailedCreateException}.
+     *
+     * @param user The user object containing user details.
+     */
     public void addUser(User user){
         UserRepresentation userRepresentation = new UserRepresentation();
         userRepresentation.setUsername(user.getLogin());
@@ -49,16 +74,23 @@ public class KeyCloakService {
         Response response = usersResource.create(userRepresentation);
 
         if (response.getStatus() == Response.Status.CREATED.getStatusCode()) {
-            System.out.println("User created successfully.");
+            log.info("User created int keycloak successfully.");
         } else {
-            System.out.println("Failed to create user. HTTP status: " + response.getStatus());
-            System.out.println("Response body: " + response.readEntity(String.class));
+            log.error("Failed to create keycloak user. HTTP status: " + response.getStatus());
+            log.error("Response body: " + response.readEntity(String.class));
+            throw new UserFailedCreateException(HttpStatus.BAD_REQUEST,"Failed to create user");
         }
 
         String userId = response.getLocation().getPath().split("/")[5];
         assignRolesToUser(userId, Collections.singletonList("USER_REALM"));
     }
 
+    /**
+     * Assigns roles to a user.
+     *
+     * @param userId   The ID of the user.
+     * @param roleNames List of role names to be assigned.
+     */
     private void assignRolesToUser(String userId, List<String> roleNames) {
         UserResource userResource = keycloak.realm(realmName).users().get(userId);
 
@@ -69,6 +101,12 @@ public class KeyCloakService {
         userResource.roles().realmLevel().add(roles);
     }
 
+    /**
+     * Updates the password for an existing user.
+     *
+     * @param login    The login (username) of the user.
+     * @param password The new password for the user.
+     */
     public void updateUserPassword(String login, String password){
         UserRepresentation userRepresentation=findUserByName(login);
         userRepresentation.setCredentials(Collections.singletonList(createPasswordCredentials(password)));
@@ -76,6 +114,11 @@ public class KeyCloakService {
         usersResource.get(userRepresentation.getId()).update(userRepresentation);
     }
 
+    /**
+     * Updates the email verification status for an existing user.
+     *
+     * @param login The login (username) of the user.
+     */
     public void updateUserStatusEmail(String login){
         UserRepresentation userRepresentation=findUserByName(login);
         userRepresentation.setEmailVerified(true);
@@ -83,9 +126,20 @@ public class KeyCloakService {
         usersResource.get(userRepresentation.getId()).update(userRepresentation);
     }
 
-    public UserRepresentation findUserByName(String userName){
+    /**
+     * Finds a user by their login
+     *
+     * @param login The username of the user.
+     * @return The UserRepresentation object for the user.
+     */
+    public UserRepresentation findUserByName(String login){
         UsersResource usersResource = keycloak.realm(realmName).users();
-        List<UserRepresentation> user = usersResource.search(userName, true);
-        return user.get(0);
+        List<UserRepresentation> users = usersResource.search(login, true);
+
+        if (users.isEmpty()) {
+            throw new UserNotFoundException(HttpStatus.NOT_FOUND, "User not found in keycloak");
+        }
+
+        return users.get(0);
     }
 }
