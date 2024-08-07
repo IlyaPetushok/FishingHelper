e944875d2525690fb8c7344dfd0f3e60ab61f236
Petushok Ilya <a3310010752@mail.ru>
2024-08-07 12:37:27 +0300

    refactor

--- fishing-helper/security-server/src/main/java/fishinghelper/security_server/service/LogoutService.java
+++ fishing-helper/security-server/src/main/java/fishinghelper/security_server/service/LogoutService.java
@@ -1,16 +1,12 @@
 package fishinghelper.security_server.service;
 
-import fishinghelper.security_server.dao.TokenRepository;
-import fishinghelper.security_server.util.JwtProvider;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Service;
@@ -36,13 +32,11 @@ public class LogoutService implements LogoutHandler {
     @Value("${spring.security.keycloak.logout.url}")
     private String logoutUrl;
 
-
     @Override
     public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
         String bearer = request.getHeader(HttpHeaders.AUTHORIZATION);
 
         if (!hasText(bearer) || !bearer.startsWith(BEARER)) {
-//            except
             System.out.println("exception");
         }
         String token=bearer.replace(BEARER, "").trim();
