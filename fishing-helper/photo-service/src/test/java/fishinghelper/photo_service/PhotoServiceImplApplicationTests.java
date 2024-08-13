8f8eee50b11f589667f967729daabee35c0fda83
Petushok Ilya <a3310010752@mail.ru>
2024-08-13 16:06:56 +0300

    add show weather for place

---
+++ fishing-helper/photo-service/src/test/java/fishinghelper/photo_service/PhotoServiceImplApplicationTests.java
@@ -0,0 +1,13 @@
+package fishinghelper.photo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoServiceImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
