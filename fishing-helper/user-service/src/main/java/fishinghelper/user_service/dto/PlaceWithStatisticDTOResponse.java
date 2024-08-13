712e9542f8301bb24a02b766f649e477ec873d73
Petushok Ilya <a3310010752@mail.ru>
2024-08-13 16:00:41 +0300

    add show weather for place

--- fishing-helper/user-service/src/main/java/fishinghelper/user_service/dto/PlaceWithStatisticDTOResponse.java
+++ fishing-helper/user-service/src/main/java/fishinghelper/user_service/dto/PlaceWithStatisticDTOResponse.java
@@ -10,4 +10,5 @@ import lombok.NoArgsConstructor;
 public class PlaceWithStatisticDTOResponse {
     private PlaceDTOResponse placeDTOResponse;
     private TimeToCatchFish timeToCatchFish;
+    private WeatherPlaceDTOResponse weatherPlaceDTOResponse;
 }
