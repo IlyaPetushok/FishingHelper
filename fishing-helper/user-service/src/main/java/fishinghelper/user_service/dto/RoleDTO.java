d91fcf07e572cd4c9d98ba4cb346756cf1ab5a1f
Petushok Ilya <a3310010752@mail.ru>
2024-08-20 02:10:34 +0300

    for front:for info user

---
+++ fishing-helper/user-service/src/main/java/fishinghelper/user_service/dto/RoleDTO.java
@@ -0,0 +1,12 @@
+package fishinghelper.user_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleDTO {
+    private String name;
+}
